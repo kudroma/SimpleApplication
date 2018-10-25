9bcdbd2fe3cc0e7a912860fad7f9535c8059f690
Anna <it.sovaanya@gmail.com>
2018-10-25 18:34:41 +0300

    counted the points on the figures

--- SimpleApplication/Circle.cpp
+++ SimpleApplication/Circle.cpp
@@ -1,38 +1,55 @@
 #include "Circle.h"
+#include "Point2d.h"
 #include "AbstractFigure.h"
+
 
 #define _USE_MATH_DEFINES
 #include <math.h>
 
+using namespace SimpleApplication;
 
-SimpleApplication::Circle::Circle()
+Circle::Circle(int id, Point2d circleCenter, float radius)
+	: AbstractFigure(id), m_center(circleCenter), m_radius(radius)
+{
+	int indexI = 0;
+
+	while (m_angle*indexI < 360)
+	{
+		auto point = coordinatesPointAtAngle(m_angle, indexI);
+		m_points.push_back(point);
+		indexI++;
+    }
+}
+
+Circle::~Circle()
 {
 }
 
-SimpleApplication::Circle::Circle(int id, const Point2d& center, float r) : AbstractFigure(id), m_radius(r), m_center(center)
-{
-}
-
-SimpleApplication::Circle::~Circle()
-{
-}
-
-float  SimpleApplication::Circle::area () const
+float Circle::area() const
 {
 	return m_radius * m_radius * static_cast<float>(M_PI);
 }
 
-float  SimpleApplication::Circle::perimeter() const
+float Circle::perimeter() const
 {
 	return 2 * m_radius * static_cast<float>(M_PI);
 }
 
-
-SimpleApplication::BoundingRect SimpleApplication::Circle::boundingRect() const
+BoundingRect Circle::boundingRect() const
 {
 	SimpleApplication::BoundingRect bRect;
 	bRect.setCenter(m_center);
-	bRect.setWidth(m_radius *2);
-	bRect.setHeight(m_radius *2);
+	bRect.setWidth(m_radius * 2);
+	bRect.setHeight(m_radius * 2);
 	return bRect;
+}
+
+Point2d Circle::coordinatesPointAtAngle(float anglePointA, int indexAi) const
+{
+	auto x = m_center.x() + m_radius * sin(indexAi*anglePointA);
+	auto y = m_center.y() + m_radius * cos(indexAi*anglePointA);
+	Point2d pointTemp;
+	pointTemp.setX(x);
+	pointTemp.setY(y);
+	return pointTemp;
 }
--- SimpleApplication/RoundedRect.cpp
+++ SimpleApplication/RoundedRect.cpp
@@ -3,17 +3,72 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-
-
-SimpleApplication::RoundedRect::RoundedRect()
+using namespace SimpleApplication;
+RoundedRect::RoundedRect(int id, const Point2d& center, float w, float h, float r) 
+	: AbstractFigure(id), m_width(w), m_height(h), m_radius(r), m_center(center)
 {
+	m_points.reserve(8);
+
+	Point2d pointTemp;
+
+	// Coordinates point 1ts
+	auto x = m_center.x - m_width / 2 + m_radius;
+	auto y = m_center.y - m_height / 2;
+	pointTemp.setX(x);
+	pointTemp.setY(y);
+	m_points.push_back(pointTemp);
+
+	// Coordinates point 2ts
+	x = m_center.x - m_width / 2;
+	y = m_center.y - m_height / 2 + m_radius;
+	pointTemp.setX(x);
+	pointTemp.setY(y);
+	m_points.push_back(pointTemp);
+
+	// Coordinates point 3ts
+	x = m_center.x - m_width / 2;
+	y = m_center.y + m_width / 2 - m_radius;
+	pointTemp.setX(x);
+	pointTemp.setY(y);
+	m_points.push_back(pointTemp);
+
+	// Coordinates point 4ts
+	x = m_center.x - m_width / 2 + m_radius;
+	y = m_center.y + m_height / 2;
+	pointTemp.setX(x);
+	pointTemp.setY(y);
+	m_points.push_back(pointTemp);
+
+	// Coordinates point 5ts
+	x = m_center.x + m_width / 2 - m_radius;
+	y = m_center.y + m_height / 2;
+	pointTemp.setX(x);
+	pointTemp.setY(y);
+	m_points.push_back(pointTemp);
+
+	// Coordinates point 6ts
+	x = m_center.x + m_width / 2;
+	y = m_center.y + m_width / 2 - m_radius;
+	pointTemp.setX(x);
+	pointTemp.setY(y);
+	m_points.push_back(pointTemp);
+
+	// Coordinates point 7ts
+	x = m_center.x + m_width / 2;
+	y = m_center.y - m_height / 2 + m_radius;
+	pointTemp.setX(x);
+	pointTemp.setY(y);
+	m_points.push_back(pointTemp);
+
+	// Coordinates point 8ts
+	x = m_center.x + m_width / 2 - m_radius;
+	y = m_center.y - m_height / 2;
+	pointTemp.setX(x);
+	pointTemp.setY(y);
+	m_points.push_back(pointTemp);
 }
 
-SimpleApplication::RoundedRect::RoundedRect(int id, const Point2d& center, float w, float h, float r) : AbstractFigure(id), m_width(w), m_height(h), m_radius(r), m_center(center)
-{
-}
-
-SimpleApplication::RoundedRect::~RoundedRect()
+RoundedRect::~RoundedRect()
 {
 }
 
@@ -27,7 +82,7 @@ float SimpleApplication::RoundedRect::area() const
 	return m_width * m_height;
 }
 
-SimpleApplication::BoundingRect SimpleApplication::RoundedRect::boundingRect() const
+BoundingRect SimpleApplication::RoundedRect::boundingRect() const
 {
 	SimpleApplication::BoundingRect bRect;
 	bRect.setCenter(m_center);
