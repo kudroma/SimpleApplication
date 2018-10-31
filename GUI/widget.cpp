bf8462108dd09f187c25e92072815ababea20f23
Zavadski Pavel <pavel4840@gmail.com>
2018-10-31 18:33:32 +0300

    Set the text in all tags on the widget

--- GUI/widget.cpp
+++ GUI/widget.cpp
@@ -6,6 +6,12 @@ Widget::Widget(QWidget *parent) :
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+	ui->comboBoxFigure->addItem(QString("Rectangle"));
+	ui->comboBoxFigure->addItem(QString("Triangle"));
+	ui->comboBoxFigure->addItem(QString("Trapeze"));
+	ui->comboBoxFigure->addItem(QString("Circle"));
+	ui->comboBoxFigure->addItem(QString("Ellipse"));
+	ui->comboBoxFigure->addItem(QString("RoundedRect"));
 }
 
 Widget::~Widget()
@@ -18,7 +24,63 @@ void Widget::setTextLabel1(const QString & text)
     ui->label_1->setText(text);
 }
 
+void Widget::setTextLabel2(const QString & text)
+{
+	ui->label_2->setText(text);
+}
+
+void Widget::setTextLabel3(const QString & text)
+{
+	ui->label_3->setText(text);
+}
+
+void Widget::setTextLabel4(const QString & text)
+{
+	ui->label_4->setText(text);
+}
+
+void Widget::setTextLabel5(const QString & text)
+{
+	ui->label_5->setText(text);
+}
+
+void Widget::setTextLabel6(const QString & text)
+{
+	ui->label_6->setText(text);
+}
+
+void Widget::setVisibleLabel1(bool visible)
+{
+	ui->label_1->setVisible(visible);
+}
+
+void Widget::setVisibleLabel2(bool visible)
+{
+	ui->label_2->setVisible(visible);
+}
+
+void Widget::setVisibleLabel3(bool visible)
+{
+	ui->label_3->setVisible(visible);
+}
+
+void Widget::setVisibleLabel4(bool visible)
+{
+	ui->label_4->setVisible(visible);
+}
+
+void Widget::setVisibleLabel5(bool visible)
+{
+	ui->label_5->setVisible(visible);
+}
+
+void Widget::setVisibleLabel6(bool visible)
+{
+	ui->label_6->setVisible(visible);
+}
+
 void Widget::setTextLineEdit1(const QString & text)
 {
     ui->lineEdit_1->setText(text);
+	
 }
