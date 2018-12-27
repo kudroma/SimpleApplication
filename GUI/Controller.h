50356d7a7fa0c83664c4886b4740dad0ce50e2c6
Igor <igoreshka.first@gmail.com>
2018-12-27 18:39:20 +0200

    dev/figure-update

--- GUI/Controller.h
+++ GUI/Controller.h
@@ -85,6 +85,21 @@ namespace GUI
         //! Updates circle on the screen.
         void updateCircle();
 	
+		//! Updates triangle on the screen.
+		void updateTriangle();
+
+		//! Updates rectangle on the screen.
+		void updateRectangle();
+
+		//! Updates ellipse on the screen.
+		void updateEllipse();
+
+		//! Updates roundedRect on the screen.
+		void updateRoundedRect();
+
+		//! Updates trapeze on the screen.
+		void updateTrapeze();
+
 	private:
 		//! View figure.
 		QWidget* m_view;
